5ea38e607063cd73dc9f6ae4b87025b5edf3be73
SaanviPrisha <69468899+SaanviPrisha@users.noreply.github.com>
2021-01-19 22:06:30 -0500

    Update TransactionScreen.js

--- Screens/TransactionScreen.js
+++ Screens/TransactionScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as Permissions from 'expo-permissions'
+import { Header } from 'react-native-elements'
 
 
 export default class TransactionScreen extends React.Component {
@@ -11,38 +12,62 @@ export default class TransactionScreen extends React.Component {
       hasCamPerm: null,
       scanned: false,
       scannedData: "",
-      buttonState: "normal"
+      buttonState: "normal",
+      scannedStudentID: "",
+      scannedBookID: ""
     }
   }
-  getCameraPermissions = async () => {
+  getCameraPermissions = async (id) => {
     const {status} = await Permissions.askAsync(Permissions.CAMERA)
     this.setState({
       hasCamPerm: status === "granted",
-      buttonState: "clicked"
+      buttonState: id, 
+      scanned: false
     })
   }
   handleBarcodeScanned = (type,data) => {
-    this.setState({
-      scannedData: data,
-      scanned: true,
-      buttonState: "normal"
-    })
+    if(this.state.buttonState == "bookId") {
+      this.setState({
+        scannedBookID: data,
+        scanned: true,
+        buttonState: "normal"
+      })
+    } else if(this.state.buttonState == "studentID") {
+      this.setState({
+        scannedStudentID: data,
+        scanned: true,
+        buttonState: 'normal'
+      })
+    }
   }
   render() {
     const hasCamPerm = this.state.hasCamPerm
     const buttonState = this.state.buttonState
     const scanned = this.state.scanned
-    if(hasCamPerm == true && buttonState == "clicked") {
+    if(hasCamPerm == true && buttonState != "normal") {
       return(
         <BarCodeScanner style={StyleSheet.absoluteFillObject} onBarCodeScanned={scanned ? undefined: this.handleBarcodeScanned}/>
       )
     } else if(buttonState == "normal") {
       return (
         <View style={styles.container}>
-          <Text>{hasCamPerm == true ? this.state.scannedData : "Request for camera permissions"}</Text>
-          <View>
-            <TextInput placeholde="Book ID" style={styles.textInput}/>
-            <TouchableOpacity onPress={this.getCameraPermissions} style={styles.scanButton}>
+          <View style={styles.center}>
+            <Header containerStyle={{justifyContent: "space-around", width: '100%'}} centerComponent={{text: "E-Library", style: {color: '#a1cced', fontSize: 25}}}></Header>
+            <Image source={require('../assets/booklogo.jpg')} style={styles.image}/>
+          </View>
+          <View style={styles.center}>
+            <TextInput placeholder="Book ID" style={styles.textInput}/>
+            <TouchableOpacity onPress={() => {
+              this.getCameraPermissions('bookID')
+            }} style={styles.scanButton}>
+            <Text style={styles.ScanButtonText}>Scan</Text>
+          </TouchableOpacity>
+          </View>
+          <View style={styles.center}>
+            <TextInput placeholder="Student ID" style={styles.textInput}/>
+            <TouchableOpacity onPress={() => {
+              this.getCameraPermissions('studentID')
+            }} style={styles.scanButton}>
             <Text style={styles.ScanButtonText}>Scan</Text>
           </TouchableOpacity>
           </View>
@@ -56,16 +81,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   scanButton: {
     backgroundColor: '#871010',
     width: 100, 
-    height: 50, 
+    height: 50,
+    marginLeft: 230,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 5
+    borderRadius: 5,
+    marginTop: -50,
   },
   ScanButtonText: {
     fontSize: 20, 
@@ -74,9 +99,20 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 2,
-    width: 100,
+    width: 200,
     height: 50,
     borderRadius: 5,
-    marginTop: 200
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 30,
+  },
+  image: {
+    width: 100, 
+    height: 100, 
+    marginTop: 30
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
